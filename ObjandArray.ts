beda2faacd885924820e53f5624434c6d8d3b682
Yamini0 <yaminis341@gmail.com>
2021-04-09 19:33:22 +0530

    Object and Array

---
+++ ObjandArray.ts
@@ -0,0 +1,14 @@
+//Arrays
+export{}
+let name = ['Harvey', 'Donna', 'Micheal'];
+name.push('Rachel');
+console.log (name);
+
+
+//Objects
+let suits = {
+    fname : 'Loius',
+    lname : 'Litt',
+    age: '29'
+
+}
